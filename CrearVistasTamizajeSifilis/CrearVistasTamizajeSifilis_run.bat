972bf37ccf848638970d23389dc75295cadc14b4
desarrollo.social.multisectorial <desarrollo.social.multisectorial@gmail.com>
2019-10-17 12:45:35 -0500

    primera carga

---
+++ CrearVistasTamizajeSifilis/CrearVistasTamizajeSifilis_run.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/crypto-utils.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.17.jar;../lib/mysql-connector-java-5.1.30-bin.jar;crearvistastamizajesifilis_0_1.jar; etl_indicadores_diresa.crearvistastamizajesifilis_0_1.CrearVistasTamizajeSifilis  --context=PruebaLocal %*
